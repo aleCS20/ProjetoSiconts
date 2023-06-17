4a65b534c7fbb692ecc4b6ca2d6be305f913446b
Alessandro Barbosa de Oliveira <59849494+aleCS20@users.noreply.github.com>
2023-06-16 22:31:09 -0300

    comentários

--- projetosiconts/public/js/actions_items.js
+++ projetosiconts/public/js/actions_items.js
@@ -1,26 +1,30 @@
 //script para adicionar apenas cartões na coluna de backlog
 //<div class="card" draggable="true"> <div class="status todo"></div> <div class="content">Dinamicamente.</div></div>
+
+//função para acrescentar um item a coluna de Backlog
 function enviar_item() {
+    //variáveis para obter o nome do formulário e o conteúdo do campo descrição
     const formTask = document.getElementById('addItem');
     const task = document.getElementById('descricao');
-
+    //evento Listener para o 'submeter' a ação de card a ser adicionado
     formTask.addEventListener('submit', (event) => {
         if (task.value != '') {
             addCard(task.value);
-            task.value = '';
+            task.value = '';//irá receber o valor de descrição
         }
         event.preventDefault();
     })
-
+    //função para adicionar o valor da descrição para criar um card 'cartão'.
     function addCard(value) {
-        const todo = document.querySelector('#backlog');
-        const newCard = document.createElement("div");
-        newCard.classList.add('card');
+        const todo = document.querySelector('#backlog');//define o local de inserção do card no dashboard
+        const newCard = document.createElement("div");//define o local de inserção do card em uma div
+        newCard.classList.add('card');//
         newCard.draggable = true;
+        //aqui o cartão é inserido na coluna backlog
         newCard.innerHTML = `
-    <div class="status backlog"></div>
-    <div class="content"><p>`+ value + `</p></div>
-  `;
+            <div class="status backlog"></div>
+            <div class="content"><p>`+ value + `</p></div>
+        `;
         newCard.addEventListener('dragstart', dragStart);
         newCard.addEventListener('drag', drag);
         newCard.addEventListener('dragend', dragEnd);
@@ -35,7 +39,7 @@ function enviar_item() {
         card.addEventListener('drag', drag);
         card.addEventListener('dragend', dragEnd);
     })
-
+    //função para iniciar o cartão
     function dragStart() {
         dropZones.forEach(dropZone => dropZone.classList.add('highlight'));
         this.classList.add('dragging');
@@ -64,7 +68,7 @@ function enviar_item() {
     function drag() {
 
     }
-
+    //função para os cartões serem devidamente adicionados as colunas
     function dragEnd() {
         dropZones.forEach(dropZone => dropZone.classList.remove('highlight'));
         this.classList.remove('dragging');
@@ -89,7 +93,7 @@ function enviar_item() {
                 break;
         }
     }
-
+    //criando for para os eventos de movimentos dos cartões - chamadas dos eventos
     dropZones.forEach(dropZone => {
         dropZone.addEventListener('dragenter', dragEnter);
         dropZone.addEventListener('dragover', dragOver);
@@ -100,7 +104,7 @@ function enviar_item() {
     function dragEnter() {
 
     }
-
+    //evento - para possibilitar o cartão ser arrastável
     function dragOver() {
         this.classList.add('over');
 
@@ -108,11 +112,11 @@ function enviar_item() {
 
         this.appendChild(cardBeingDragged);
     }
-
+    //evento - para possibilitar o cartão ser movido
     function dragLeave() {
         this.classList.remove('over');
     }
-
+    //evento - para possibilitar o cartão ser solto na coluna
     function drop() {
         this.classList.remove('over');
     }
